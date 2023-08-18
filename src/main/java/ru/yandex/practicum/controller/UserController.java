ca606e950e6d9d0100f3d625c627b9a6dba6be9c
Armeha <armeha0203@yandex.ru>
2023-08-18 12:50:09 +0300

    feat: тз9

--- src/main/java/ru/yandex/practicum/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -26,6 +26,9 @@ public class UserController {
 
     @PostMapping
     public User postUser(@Valid @RequestBody User user) {
+        if (user.getName() == null || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
         user.setId(idUser++);
         integerUserMap.put(user.getId(), user);
         log.info("добавление пользователя");
