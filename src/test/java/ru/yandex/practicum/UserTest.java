9f14da89d57b5479a24ff3286aceec27d1bc178f
Armeha <armeha0203@yandex.ru>
2023-08-18 11:50:18 +0300

    feat: тз9

--- src/test/java/ru/yandex/practicum/UserTest.java
+++ src/test/java/ru/yandex/practicum/UserTest.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum;
 
-import lombok.Value;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
